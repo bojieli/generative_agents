57df57a49ebc82271d652c28b866f489f84146c5
Siyuan <suquark@gmail.com>
2023-08-25 16:50:23 -0700

    migrate storage path

--- gen_agents/utils.py
+++ gen_agents/utils.py
@@ -9,8 +9,8 @@ maze_assets_loc = os.path.join(ROOT_DIR, "frontend_server/static_dirs/assets")
 env_matrix = f"{maze_assets_loc}/the_ville/matrix"
 env_visuals = f"{maze_assets_loc}/the_ville/visuals"
 
-fs_storage = os.path.join(ROOT_DIR, "frontend_server/storage")
-fs_temp_storage = os.path.join(ROOT_DIR, "frontend_server/temp_storage")
+fs_storage = os.path.join(ROOT_DIR, "storage")
+fs_temp_storage = os.path.join(ROOT_DIR, "temp_storage")
 
 collision_block_id = "32125"
 
